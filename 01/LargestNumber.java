e5ac111f03fa709f1669d8aca01cbdfe5bf568e5
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 04:32:30 +0000

    Who is the largest number?

---
+++ 01/LargestNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers do you want to compare? ");
+        int N = sc.nextInt();
+        int currentLargestNumber = Integer.MIN_VALUE;
+        for (int i = 0; i<N; i++) {
+            System.out.print("Type a number: ");
+            int currentNumber = sc.nextInt();
+            currentLargestNumber =  currentNumber > currentLargestNumber ? currentNumber : currentLargestNumber;
+        }
+        sc.close();
+        System.out.println("The largest number is: " + currentLargestNumber);
+       
+    }
+}
