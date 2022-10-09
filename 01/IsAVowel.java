b9023027232f4db0925b9eb333c7d40917613685
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 02:33:37 +0000

    is Vowel?

---
+++ 01/IsAVowel.java
@@ -0,0 +1,19 @@
+public class IsAVowel {
+    public static void main(String[] args) {
+        System.out.println(testLetter("e")); // Vowel
+        System.out.println(testLetter("a")); // Vowel
+        System.out.println(testLetter("t")); // Consonant
+        System.out.println(testLetter("O")); // Vowel 
+    }
+
+    public static String testLetter(String l) {
+        switch (l.toLowerCase()) {
+            case "a": return "Vowel";
+            case "e": return "Vowel";
+            case "i": return "Vowel";
+            case "o": return "Vowel";
+            case "u": return "Vowel";
+            default: return "Consonant";
+        }
+    }
+}
