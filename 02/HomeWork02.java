6fac26e8f001a0088b0bda8a77363dec1e7ae838
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 15:18:10 +0000

    home work 02

---
+++ 02/HomeWork02.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class HomeWork02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] nums = new int[5];
+        System.out.print("Type the array1's values: ");
+        for (int j = 0; j<5; j++) {
+            nums[j] = sc.nextInt();
+        }
+
+        int[] nums2 = new int[5];
+        System.out.print("Type the array2's values: ");
+        for (int k = 0; k<5; k++) {
+            nums2[k] = sc.nextInt();
+        }
+        sc.close();
+
+        int[] nums3 = new int[5];
+        int l = 0;
+        for (int n1 : nums) {
+            for (int n2 : nums2) {
+                if (n1 == n2) {
+                    nums3[l] = n1;
+                }
+            }
+            l += 1;
+        }
+
+        for (int n3 : nums3) {
+            if (n3 != 0) {
+                System.out.println(n3);
+            }
+        }
+    }
+}
