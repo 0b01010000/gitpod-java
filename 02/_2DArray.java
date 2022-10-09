2aacfbbbcb7baaff4ab2a6ad686c4e91fff6d05e
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 20:28:18 +0000

    _2d array

---
+++ 02/_2DArray.java
@@ -0,0 +1,28 @@
+public class _2DArray {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 0, 2, -3},
+            {4, 0, 3, 2, -7},
+            {-10, 4, 5, -1}
+        };
+
+        System.out.println("[");
+        for (int j = 0; j<a.length; j++) {
+            System.out.print("  [");
+            for (int i = 0; i<a[j].length; i++) {
+                if ((i + 1) == a[j].length) {
+                    System.out.print(a[j][i]);
+                } else {
+                    System.out.print(a[j][i] + ", ");
+                }
+            
+            }
+            if ((j + 1) == a.length) {
+                System.out.println("]");
+            } else {
+                System.out.println("],");
+            }
+        }
+        System.out.println("]");
+    }    
+}
