fd5d55f0d122f8c692302952222769d4fc166e26
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 14:39:18 +0000

    enhanced for loop

---
+++ 02/ForEachLoop.java
@@ -0,0 +1,9 @@
+class ForEachLoop {
+    public static void main(String[] args) {
+        int[] nums = {5, 4, 4, 7, 7, 1, 69};
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
