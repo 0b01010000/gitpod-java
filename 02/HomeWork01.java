e04c24d3b20bc16e6b6bf32be3849b5b18a298bf
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 15:13:27 +0000

    home work 01

---
+++ 02/HomeWork01.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class HomeWork01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        int[] nums = new int[N];
+        for (int i = 0; i<N; i++) {
+            nums[i] = (i+1) * (i+1);
+        }
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+
+    }
+}
