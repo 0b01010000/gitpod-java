63ea73e9909f93d3824f8b281dfc54b74e2504ac
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-09 00:51:46 +0000

    Day 01

---
+++ 01/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(rev("amoR")); // Roma
+        System.out.println(rev("livE")); // Evil
+    }
+
+    public static String rev(String s) {
+        int size = s.length();
+        String str = "";
+
+        int i = size - 1;
+        while (i>=0) {
+            str += s.charAt(i);
+            i -= 1;
+        }
+        return str;
+    }
+}
