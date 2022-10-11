301c8cf20597839363fcbecca02bbe24c5d70d25
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-11 01:03:18 +0000

    sample factorial

---
+++ up_03/Factorial.java
@@ -0,0 +1,21 @@
+package up_03;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    static int fact(int n) {
+        int product = 1;
+        for (int i = n; i>1; i--) {
+            product *= i;
+        }
+        return product;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println("The factorial of " + n + " is " + fact(n) + ".");
+    }
+}
