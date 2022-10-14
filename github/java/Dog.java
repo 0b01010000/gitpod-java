94a1412f3bcea8360a8f7131662e3b9910c12f6a
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-14 16:52:40 +0000

    exercise ehchua's class: escaping characters

---
+++ github/java/Dog.java
@@ -0,0 +1,14 @@
+package github.java;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        System.out.println("          '__'");
+        System.out.println("          (oo)");
+        System.out.println("  +========\\/");
+        System.out.println(" / || %%% ||");
+        System.out.println("*  ||-----||");
+        System.out.println("   \"\"     \"\"");
+    }
+    
+}
