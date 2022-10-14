ca56eaac408badf23b2fc995735fbccbe0f0e0f2
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-14 16:07:19 +0000

    exercise variable names for ehchua online class

---
+++ github/java/VariableNamesAndTypes.java
@@ -0,0 +1,28 @@
+package github.java;
+
+public class VariableNamesAndTypes {
+    public static void main(String[] args) {
+        String name = "Paul";
+        String address = "Back Street 34";
+        String phoneNumber = "+55(00)00000-0000";
+        char gender = 'M';
+        String dateOfBirth = "02/03/1991";
+       // double height = 1.80;
+      //  double weight = 71.5;
+        String degreePursued = "B.Sc";
+     //   int yearOfStudy = 2022;
+     //   double averageGPA = 9.8;
+      //  boolean hasTuitionGrant = true;
+      //  boolean isAScholar = true;
+        byte studentID = 121;
+
+        System.out.println("Student's Info.");
+        System.out.println("Name: " + name);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Degree Pursued: " + degreePursued);
+        System.out.println("Address: " + address);
+        System.out.println("Gender: " + gender);
+        System.out.println("Birth: " + dateOfBirth);
+        System.out.println("Student ID: " + studentID);
+    }   
+}
