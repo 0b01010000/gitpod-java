188ad6013c6521cba6bba0210fc58728388a9a81
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-15 02:02:06 +0000

    online class reverse str

---
+++ ReverseStr.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseStr {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Type a word: ");
+        String str = in.nextLine();
+        in.close();
+
+        String reversedStr = "";
+        int i = str.length() - 1;
+        while (i >= 0) {
+            reversedStr += str.charAt(i);
+            i -= 1;
+        }
+        System.out.printf("The reverse is: %s. %n", reversedStr);
+    }
+}
