267e24b9362e671534375df40bb488e0d13a13d4
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-16 06:17:36 +0000

    add exercises from ehchua online class

---
+++ github/java/CheckOddEven.java
@@ -0,0 +1,15 @@
+package github.java;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+        int number = 12;
+
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+
+        System.out.println("bye!");
+    }
+}
